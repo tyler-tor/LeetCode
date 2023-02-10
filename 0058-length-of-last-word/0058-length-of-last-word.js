fb79c87ef1c64e9a983051ea7a611bb2489b6a88
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-02-10 11:13:38 -0800

    Time: 63 ms (69.06%), Space: 42 MB (48.87%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,10 +4,8 @@
  */
 var lengthOfLastWord = function(s) {
     let words = s.split(' ');
-    console.log(words)
     while(words[words.length - 1] === ''){
         words.pop()
     }
-    let last = words.pop();
-    return last.length
+    return words.pop().length;
 };
