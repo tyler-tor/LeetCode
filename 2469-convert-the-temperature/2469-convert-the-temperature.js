ccffdb2d17cfcaeea6e60dc0f2efb8d9e70e5fc8
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-03-06 20:27:28 -0800

    Time: 48 ms (98.46%), Space: 41.7 MB (82.25%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -3,7 +3,5 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    const kelvin = celsius + 273.15;
-    const farenheit = (celsius * 1.8) + 32;
-    return [kelvin, farenheit];
+    return [(celsius + 273.15), ((celsius * 1.8) + 32)];
 };
