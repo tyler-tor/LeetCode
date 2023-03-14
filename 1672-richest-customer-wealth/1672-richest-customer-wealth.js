36368c6cfdb5f37d381fc95bc85c01721bdad795
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-03-14 15:35:41 -0700

    Time: 67 ms (41.64%), Space: 42.1 MB (49.50%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -4,18 +4,21 @@
  */
 var maximumWealth = function(accounts) {
     let ammount = -Infinity;
-    let checkAmmount = 0;
+    // let checkAmmount = 0;
     
     for(let i = 0; i < accounts.length; i++) {
         let customer = accounts[i];
         
-        for(let j = 0; j < customer.length; j++) {
-           let num = customer[j];
-            checkAmmount += num;
-        };
+        // const initialValue = 0;
+        let checkAmmount = customer.reduce((accumulator, currentValue) => accumulator + currentValue);
+        
+        // for(let j = 0; j < customer.length; j++) {
+        //    let num = customer[j];
+        //     checkAmmount += num;
+        // };
         if(checkAmmount > ammount) ammount = checkAmmount;
         
-        checkAmmount = 0;
+        // checkAmmount = 0;
     }
     return ammount;
 };
