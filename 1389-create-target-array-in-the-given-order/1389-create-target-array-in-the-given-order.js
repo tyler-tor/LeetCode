0c37780707183d61f62032992885d64449aae224
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-03-20 15:36:13 -0700

    Time: 61 ms (67.43%), Space: 42.1 MB (55.59%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -5,9 +5,9 @@
  */
 var createTargetArray = function(nums, index) {
     let target = [];
-    let length = nums.length > index.length ? nums.length : index.length;
+    // let length = nums.length > index.length ? nums.length : index.length;
     
-    for(let i = 0; i < length; i++) {
+    for(let i = 0; i < nums.length; i++) {
         target.splice(index[i], 0, nums[i])
     }
     
