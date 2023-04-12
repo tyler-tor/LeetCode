0a6e1f4104c2c739f79fb687dadad41d2de39e7c
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-04-12 13:08:39 -0700

    Time: 57 ms (50.00%), Space: 41.9 MB (50.00%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    let promise = await new Promise((resolve, reject) => {
+         setTimeout(() => {
+            resolve()
+        }, millis)
+    })  
+    
+    return promise
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
