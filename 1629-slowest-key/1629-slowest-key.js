7947f69662a4d90e5624c5340e5b63d0dd72f32d
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-04-24 11:17:39 -0700

    Time: 68 ms (53.01%), Space: 45.1 MB (16.87%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.js
+++ 1629-slowest-key/1629-slowest-key.js
@@ -5,30 +5,22 @@
  */
 var slowestKey = function(releaseTimes, keysPressed) {
     let resObj = {};
-    let pastTime = 0
+    let pastTime = 0;
+    let max = 0;
     
     for(let i = 0; i < releaseTimes.length; i++) {
         let time = releaseTimes[i] - pastTime;
-        let key = keysPressed[i];
         
-        if(resObj[key]) {
-            if(time > resObj[key]) {
-                resObj[key] = time
+        if(resObj[time]) {
+            if(keysPressed[i] > resObj[time]) {
+                resObj[time] = keysPressed[i]
             }
         }else {
-            resObj[key] = time;
+            resObj[time] = keysPressed[i];
         }
-        
+        if(time > max) max = time
         pastTime = releaseTimes[i];
     };
-    
-    let letter = Object.keys(resObj).reduce((a, b) => {
-        if(resObj[a] === resObj[b]){
-            return a > b ? a : b;
-        }else {
-            return resObj[a] > resObj[b] ? a : b;
-        }
-    });
 
-    return letter
+    return resObj[max]
 };
