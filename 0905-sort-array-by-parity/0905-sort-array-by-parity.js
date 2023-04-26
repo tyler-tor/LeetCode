c219c3b493337a6be4bd8b9430d1dfed06ca94e4
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-04-26 06:33:48 -0700

    Time: 92 ms (6.67%), Space: 44.2 MB (83.38%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -5,8 +5,7 @@
 var sortArrayByParity = function(nums) {
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] % 2 === 0) {
-            let even = nums.splice(i, 1);
-            nums.unshift(even)
+            nums.unshift(nums.splice(i, 1))
         }
     };
     return nums;
