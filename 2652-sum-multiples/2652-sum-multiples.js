5cfac550f04bb148ccbfdb6312a7c850bf2eb898
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-04-26 07:02:33 -0700

    Time: 87 ms (9.76%), Space: 43.3 MB (83.23%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -5,7 +5,7 @@
 var sumOfMultiples = function(n) {
     let num = 0;
     
-    for(let i = 1; i <= n; i++){
+    for(let i = 3; i <= n; i++){
         if(i % 3 === 0 || i % 5 === 0 || i % 7 === 0) num += i;
     };
     
