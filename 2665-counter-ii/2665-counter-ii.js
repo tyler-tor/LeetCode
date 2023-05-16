1595d6c3745d78d80e07016ee2c075b0bd5d0865
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-05-16 08:30:52 -0700

    Time: 68 ms (55.73%), Space: 44.4 MB (79.13%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let c = init;
+    return {
+        increment () {
+            return ++c;
+        },
+        decrement () {
+            return --c;
+        },
+        reset () {
+            c = init;
+            return c;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
