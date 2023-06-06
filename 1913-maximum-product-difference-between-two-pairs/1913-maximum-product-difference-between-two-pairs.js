2c3690a33797c452796bc453cfc7195c259f6995
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-06-06 11:32:53 -0700

    Time: 109 ms (8.33%), Space: 48.1 MB (5.65%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    let sorted = nums.sort((a,b) => a - b);
+    let min = sorted[0] * sorted[1];
+    let max = sorted[sorted.length - 1] * sorted[sorted.length - 2];
+    console.log(sorted)
+    return max - min;
+};
+
+maxProductDifference([1,6,7,5,2,4,10,6,4])
