a607a3928db615005b104347ef3b5859ef88a3aa
Derek "Tyler" Torrero <93111660+tyler-tor@users.noreply.github.com>
2023-10-13 12:20:07 -0600

    Time: 38 ms (99.52%), Space: 42.4 MB (12.54%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let string = '';
+    let curr = head
+    
+    while(curr) {
+        string += `${curr.val}`;
+        curr = curr.next;
+    };
+
+    return parseInt(string, 2)
+};
